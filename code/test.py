e77de794852c1a63ad3d1203fcecb3d7199d2461
maohongqi <maohongqi@qianxin.com>
2019-12-26 18:44:44 +0800

    hello

---
+++ code/test.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.pensize(10)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
