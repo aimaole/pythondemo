8623eedb35308c2e7ba6d81d552f15785e109884
maohongqi <maohongqi@qianxin.com>
2020-01-03 17:14:25 +0800

    hello

--- code/hello.py
+++ code/hello.py
@@ -1,1 +1,90 @@
 print("hello")
+"""
+使用变量保存数据并进行算术运算
+
+Version: 0.1
+Author: 骆昊
+"""
+
+a = 321
+b = 123
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+"""
+使用type()检查变量的类型
+
+Version: 0.1
+Author: 骆昊
+Date: 2018-02-27
+"""
+
+a = 100
+b = 12.345
+c = 1 + 5j
+d = 'hello, world'
+e = True
+print(type(a)) # <class 'int'>
+print(type(b)) # <class 'float'>
+print(type(c)) # <class 'complex'>
+print(type(d)) # <class 'str'>
+print(type(e)) # <class 'bool'>
+
+"""
+使用input()函数获取键盘输入(字符串)
+使用int()函数将输入的字符串转换成整数
+使用print()函数输出带占位符的字符串
+
+Version: 0.1
+Author: 骆昊
+"""
+
+a = int(input('a = '))
+b = int(input('b = '))
+print('%d + %d = %d' % (a, b, a + b))
+print('%d - %d = %d' % (a, b, a - b))
+print('%d * %d = %d' % (a, b, a * b))
+print('%d / %d = %f' % (a, b, a / b))
+print('%d // %d = %d' % (a, b, a // b))
+print('%d %% %d = %d' % (a, b, a % b))
+print('%d ** %d = %d' % (a, b, a ** b))
+
+"""
+赋值运算符和复合赋值运算符
+
+Version: 0.1
+Author: 骆昊
+"""
+
+a = 10
+b = 3
+a += b # 相当于：a = a + b
+a *= a + 2 # 相当于：a = a * (a + 2)
+print(a) # 想想这里会输出什么
+
+"""
+比较、逻辑和算身份运算符的使用
+
+Version: 0.1
+Author: 骆昊
+"""
+
+flag0 = 1 == 1
+flag1 = 3 > 2
+flag2 = 2 < 1
+flag3 = flag1 and flag2
+flag4 = flag1 or flag2
+flag5 = not (1 != 2)
+print('flag0 =', flag0) # flag0 = True
+print('flag1 =', flag1) # flag1 = True
+print('flag2 =', flag2) # flag2 = False
+print('flag3 =', flag3) # flag3 = False
+print('flag4 =', flag4) # flag4 = True
+print('flag5 =', flag5) # flag5 = False
+print(flag1 is True) # True
+print(flag2 is not False) # False
